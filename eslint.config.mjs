c9d5fda9a2d5d2bb8d48d7cf50ff731c7bd41b02
ahibis <nikitonvg@list.ru>
2024-11-25 02:12:55 +0300

    refactor: eslint config change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,22 @@
 import eslintJs from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import prettierPlugin from 'eslint-plugin-prettier'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(
   {
     plugins: {
       '@typescript-eslint': tseslint.plugin,
-      prettier: prettierPlugin,
     },
   },
   {
-    ignores: ['build', 'node_modules', 'coverage', 'eslint.config.js'],
+    ignores: ['build', 'node_modules', 'eslint.config.js', 'src/prevVersion'],
   },
   eslintJs.configs.recommended,
-  // tseslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -30,8 +28,6 @@ export default tseslint.config(
   {
     files: ['**/*.{ts,tsx}'],
     rules: {
-      // ...prettierPlugin.configs.recommended.rules,
-      ...eslintConfigPrettier.rules,
       'prefer-const': 'error',
       'max-lines': ['warn', { max: 300 }],
       'max-params': ['warn', 3],
@@ -43,16 +39,31 @@ export default tseslint.config(
       'no-unmodified-loop-condition': 'warn',
       'no-unreachable-loop': 'error',
       camelcase: 'warn',
-      // complexity: ['warn', { max: 5 }],
+      complexity: ['warn', { max: 5 }],
       curly: 'warn',
       'default-case': 'warn',
       'default-case-last': 'warn',
       'dot-notation': 'warn',
       eqeqeq: 'warn',
       'func-name-matching': 'warn',
+      'max-classes-per-file': ['warn', 1],
+      'max-depth': ['warn', 3],
       '@typescript-eslint/prefer-for-of': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-empty-function': 'off',
+      // 'max-lines-per-function': ['warn', { max: 50 }],
+      'new-cap': 'error',
+      // 'no-magic-numbers': 'error',
+      'no-multi-assign': 'error',
+      'no-negated-condition': 'warn',
+      'no-nested-ternary': 'warn',
+      'no-return-assign': 'error',
+      'prettier/prettier': [
+        'off',
+        {
+          endOfLine: 'auto',
+        },
+      ],
     },
   }
 )
